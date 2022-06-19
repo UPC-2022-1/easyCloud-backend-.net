6b55d5981e29f19b956b4e2b6230369cb5897c9c
Williamz21 <u201816224@upc.edu.pe>
2022-06-19 00:20:25 -0500

    feat: security domain and exceptions implemented

---
+++ easyCloud/Security/Domain/Services/Communication/AuthenticateRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace easyCloud.Security.Domain.Services.Communication;
+
+public class AuthenticateRequest
+{
+    [Required]
+    public string Email { get; set; }
+    [Required]
+    public string Password { get; set; }
+}
